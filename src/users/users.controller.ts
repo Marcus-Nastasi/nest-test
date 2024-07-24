00d71377f25bd748cc10993a23f36b8cc54c2a30
rolemberg <vinnie.nsts@gmail.com>
2024-07-24 16:24:48 -0300

    created restful api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { RegisterDTO, UsersService } from './users.service';
 
 @Controller('users')
@@ -14,4 +14,15 @@ export class UsersController {
    async create(@Body() data: RegisterDTO) {
       return this.service.create(data);
    }
+
+   @Put('update/:id')
+   async update(@Param('id') id: number, @Body() data: RegisterDTO) {
+      return this.service.update(id, data);
+   }
+
+   @Delete('delete/:id')
+   async delete(@Param('id') id: number) {
+      return this.service.delete(id);
+   }
 }
+
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -17,4 +17,12 @@ export class UsersService {
    async create(data: RegisterDTO) {
       return this.prisma.users.create({ data });
    }
+
+   async update(id: number, data: RegisterDTO) {
+      return this.prisma.users.update({ where: { id: Number(id) }, data: data })
+   }
+
+   async delete(id: number) {
+      return this.prisma.users.delete({ where: { id: Number(id) } });
+   }
 }
