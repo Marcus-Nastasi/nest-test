833324ba62840bb4529def22a4ec8a55455c06ab
rolemberg <vinnie.nsts@gmail.com>
2024-07-30 01:00:35 -0300

    refactoring auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,15 +11,19 @@ export class AuthController {
 
    @Post('login')
    async login(@Body() data: IUser, @Res() res: Response) {
-      const user = await this.prisma.users.findUnique({ where: { id: Number(data.id) } });
-      if (!user) return res
-          .status(401)
-          .json({ status: HttpStatus.UNAUTHORIZED, message: 'Invalid user' })
-          .end();
-      return res
-          .status(HttpStatus.ACCEPTED)
-          .json({ token: this.service.generate(user.cpf) })
-          .end();
+      try {
+         const user = await this.prisma.users.findFirst({ where: { name: data.name, cpf: data.cpf } });
+         if (!user || user.name !== data.name || user.cpf !== data.cpf) return res
+             .status(401)
+             .json({ status: HttpStatus.UNAUTHORIZED, message: 'Invalid user' })
+             .end();
+         return res
+             .status(HttpStatus.ACCEPTED)
+             .json({ token: this.service.generate(user.cpf) })
+             .end();
+      } catch (e: any) {
+         return res.status(500).end();
+      }
    }
 }
 
