97f7bf9a806d34845218b708174788f129999f74
rolemberg <vinnie.nsts@gmail.com>
2024-08-03 10:58:12 -0300

    creating simple crud on purchases

--- src/purchases/purchases.controller.ts
+++ src/purchases/purchases.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { PurchasesService } from './purchases.service';
+import Purchase from 'src/Interfaces/Purchase';
 
 @Controller('purchases')
-export class PurchasesController {}
+export class PurchasesController {
+   constructor(private readonly service: PurchasesService) {}
+
+   @Get()
+   async getPurchases() {
+      return await this.service.getPurchases();
+   }
+
+   @Post('register')
+   async register(data: Purchase) {
+      return await this.service.register(data);
+   }
+
+   @Put('update/:id')
+   async update(@Param('id') id: string, data: Purchase) {
+      return await this.service.update(Number(id), data);
+   }
+
+   @Delete('delete/:id')
+   async delete(@Param('id') id: string) {
+      return await this.service.delete(Number(id));
+   }
+}
+
